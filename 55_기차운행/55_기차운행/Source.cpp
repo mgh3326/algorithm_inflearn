dc5cd179b6b0a6f42c847cbaa3d9e34ad59b645d
kwanghyunMoon <mgh3326@naver.com>
2019-08-05 23:43:59 +0900

    temp update

--- 55_기차운행/55_기차운행/Source.cpp
+++ 55_기차운행/55_기차운행/Source.cpp
@@ -2,5 +2,43 @@
 #include<stack>
 using namespace std;
 int main(void) {
+	int num = 0;
+	scanf("%d", &num);
+	stack<int> s;
+	int* arr = new int[num];
+	int findNum = 1;
+	int temp = 0;
+	string resultStr = "";
+	for (int i = 0; i < num; i++)
+	{
+		scanf("%d", &temp);
+		if (temp > findNum) {
+			if (s.size() == 0)
+			{
+				s.push(temp);
+				resultStr += "P";
+			}
+			else {
+				if (temp == s.top())
+				{
+
+				}
+				else {
+					resultStr = "Impossible";
+					break;
+				}
+			}
+		}
+		else if (temp < findNum) {
+			//실패 한 경우
+			resultStr = "Impossible";
+			break;
+		}
+		else {
+			resultStr += "PO";
+
+		}
+	}
+	cout << resultStr << endl;
 	return 0;
 }
