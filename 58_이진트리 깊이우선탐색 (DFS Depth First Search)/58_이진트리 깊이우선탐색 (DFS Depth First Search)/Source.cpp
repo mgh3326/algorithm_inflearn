3f81d9ce96869d2141b56c778e06476b6bf230d7
kwanghyunMoon <mgh3326@naver.com>
2019-08-07 00:00:19 +0900

    update

--- 58_이진트리 깊이우선탐색 (DFS Depth First Search)/58_이진트리 깊이우선탐색 (DFS Depth First Search)/Source.cpp
+++ 58_이진트리 깊이우선탐색 (DFS Depth First Search)/58_이진트리 깊이우선탐색 (DFS Depth First Search)/Source.cpp
@@ -1,2 +1,56 @@
 #include<iostream>
 using namespace std;
+void frontCircuit(int num, int limit, int* arr)
+{
+	if (num >= limit)
+	{
+		return;
+	}
+	else {
+		cout << arr[num] << " ";
+		frontCircuit(2 * num + 1, limit, arr);
+		frontCircuit(2 * num + 2, limit, arr);
+	}
+}
+void midCircuit(int num, int limit, int* arr)
+{
+	if (num >= limit)
+	{
+		return;
+	}
+	else {
+		midCircuit(2 * num + 1, limit, arr);
+		cout << arr[num] << " ";
+		midCircuit(2 * num + 2, limit, arr);
+	}
+}
+void rearCircuit(int num, int limit, int* arr)
+{
+	if (num >= limit)
+	{
+		return;
+	}
+	else {
+		rearCircuit(2 * num + 1, limit, arr);
+		rearCircuit(2 * num + 2, limit, arr);
+		cout << arr[num] << " ";
+	}
+}
+int main(void) {
+	int num = 7;
+	int* arr = new int[num];
+	for (int i = 0; i < num; i++)
+	{
+		arr[i] = i + 1;
+	}
+	cout << "전위순회 출력 : ";
+	frontCircuit(0, num, arr);
+	cout << endl;
+	cout << "중위순회 출력 : ";
+	midCircuit(0, num, arr);
+	cout << endl;
+	cout << "후위순회 출력 : ";
+	rearCircuit(0, num, arr);
+	cout << endl;
+	return 0;
+}
